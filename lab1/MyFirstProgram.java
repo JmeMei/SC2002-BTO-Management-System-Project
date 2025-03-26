a6955a447ae4d55375ecde2d9d50f67cfed295c2
Jamie <esguerra.jamie@yahoo.com>
2025-03-26 17:10:17 +0800

    Add MyFirstProgram.java

--- lab1/MyFirstProgram.java
+++ lab1/MyFirstProgram.java
@@ -4,7 +4,7 @@ public class MyFirstProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello! This is my first program.");
+		System.out.println("Hello!");
 		System.out.println("Bye Bye!");
 	}
 }
