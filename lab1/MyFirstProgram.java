de7dc2a2988eac2dc6790ff0b2624bc28db4b38b
JmeMei <76395451+JmeMei@users.noreply.github.com>
2025-03-26 15:39:02 +0800

    Update MyFirstProgram.java

--- lab1/MyFirstProgram.java
+++ lab1/MyFirstProgram.java
@@ -5,6 +5,6 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello! This is my first program.");
-		System.out.println("Bye Bye!!!");
+		System.out.println("Bye Bye!");
 	}
 }
