fdea66440dfe3d98109a4b0c6e3635eb5a27ed02
James <jameslumyewji@gmail.com>
2025-04-03 09:40:09 +0800

    added to test

--- BTOManagementSystem/HelloWorld.java
+++ BTOManagementSystem/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello, world!!");
         System.out.println("Jamie");
         System.out.println("zhtest");
+        System.out.println("james test");
     }
 }
