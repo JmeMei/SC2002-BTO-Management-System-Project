eba54b62231bc955f63e0a0209a4dc953775cfb4
sashimiwarrior <67115057+sashimiwarrior@users.noreply.github.com>
2025-04-21 21:42:17 +0800

    WithdrawalRequest Functions

--- BTOManagementSystem/Model/DAO/WithdrawalRequestDAO.java
+++ BTOManagementSystem/Model/DAO/WithdrawalRequestDAO.java
@@ -1,11 +1,10 @@
 package BTOManagementSystem.Model.DAO;
 
 import BTOManagementSystem.Model.OfficerRegistrationRequest;
+import BTOManagementSystem.Model.Project;
 import BTOManagementSystem.Model.WithdrawalRequest;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class WithdrawalRequestDAO {
@@ -26,9 +25,10 @@ public class WithdrawalRequestDAO {
 
             while ((line = br.readLine()) != null) {
 
-                String[] values = line.split(",");
+                ArrayList<String> values = CSV_data_Parse(line);
 
-                this.withdrawalRequests.add(new WithdrawalRequest(values[0], values[1], values[2], values[3]));
+                WithdrawalRequest withdrawalRequest = new WithdrawalRequest(values.get(0),values.get(1),values.get(2),values.get(3));
+                this.withdrawalRequests.add(withdrawalRequest);
 
             }
         } catch (IOException e) {
@@ -38,6 +38,54 @@ public class WithdrawalRequestDAO {
 
     }
 
+
+    private ArrayList<String> CSV_data_Parse(String line){
+
+        ArrayList<String> formatted = new ArrayList<>();
+
+        boolean inQuote = false;
+        StringBuilder aData = new StringBuilder();
+
+        for (int i = 0; i < line.length(); i++) {
+
+            char c = line.charAt(i);
+
+            if (c == '"'){
+
+                if (!inQuote){
+
+                    inQuote = true;
+
+                }else{
+                    inQuote = false;
+                }
+
+            }
+            if (c == ','){
+
+
+
+                if (!inQuote){
+                    formatted.add(aData.toString());
+
+                    aData.setLength(0);
+
+                }else{
+                    aData.append(c);
+                }
+
+            }else{
+                aData.append(c);
+            }
+
+
+        }
+
+        formatted.add(aData.toString());
+
+        return formatted;
+
+    }
 
     public ArrayList<WithdrawalRequest> getUnhandledWithdrawalRequests() {
 
@@ -54,6 +102,51 @@ public class WithdrawalRequestDAO {
         return requests;
     }
 
+
+    public void UpdateDB(){
+
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            // Write header
+            writer.write(String.join(",", Headers));
+            writer.newLine();
+
+            // Write records
+            for (WithdrawalRequest w : this.withdrawalRequests) {
+                writer.write(String.format("%s,%s,%s,%s",
+
+                                w.getName(),
+                                w.getNric(),
+                                w.getProjectName(),
+                                w.getProcessed()
+                        )
+                );
+                writer.newLine();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void CreateNewWithdrawalRequest(WithdrawalRequest request){
+
+        this.withdrawalRequests.add(request);
+        this.UpdateDB();
+
+    }
+
+    public static void main(String[] args) {
+
+        WithdrawalRequest r = new WithdrawalRequest("John","S1234567A","Acacia Breeze","0");
+        WithdrawalRequestDAO dao = new WithdrawalRequestDAO();
+        dao.CreateNewWithdrawalRequest(r);
+
+
+    }
 
 
 
--- BTOManagementSystem/Model/WithdrawalRequest.java
+++ BTOManagementSystem/Model/WithdrawalRequest.java
@@ -46,4 +46,6 @@ public class WithdrawalRequest {
         this.ProjectName = ProjectName;
     }
 
+
+
 }
